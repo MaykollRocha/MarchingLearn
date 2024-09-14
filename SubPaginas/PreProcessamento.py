73cd286820046b3dfd89cc1e3c5b8d0b71bca537
Maykoll Rocha <maykoll1412@gmail.com>
2024-09-14 12:02:23 -0400

    Adicição da primeiro SubPagina e Informais da Main principal

---
+++ SubPaginas/PreProcessamento.py
@@ -0,0 +1,32 @@
+
+import streamlit as st
+
+
+def Metricas():
+    pass
+
+def main():
+    st.title('Metricas')
+    
+    st.markdown("""
+    ### A Importância do Pré-processamento de Dados
+
+    O pré-processamento de dados é uma etapa fundamental no desenvolvimento de modelos de **Machine Learning**. Antes de treinar qualquer modelo, os dados devem ser cuidadosamente preparados para garantir que estejam em um formato adequado para análise. Dados brutos costumam conter inconsistências, valores ausentes e variáveis irrelevantes, o que pode comprometer a precisão e a performance do modelo.  
+
+    Ao realizar o pré-processamento, garantimos que os dados estejam limpos, consistentes e adequadamente transformados para atender aos requisitos específicos do algoritmo a ser utilizado. Esse processo é essencial para melhorar a qualidade dos dados, evitar erros durante o treinamento do modelo e, por fim, garantir resultados mais precisos e confiáveis.  
+
+    A seguir, são descritas as etapas principais envolvidas no pré-processamento de dados.  
+
+    ### Etapas do Pré-processamento de Dados
+
+    1. **Limpeza de dados**:  
+    Remoção de valores ausentes, duplicatas e outliers. Esse passo é crucial para evitar que dados incorretos ou distorcidos influenciem negativamente o modelo.  
+
+    2. **Transformação de dados**:  
+    Ajuste dos dados por meio de normalização ou padronização, dependendo do algoritmo escolhido. A transformação garante que os dados estejam na mesma escala ou formato, facilitando a convergência do modelo e o processamento eficiente.
+
+    3. **Engenharia de características (Feature Engineering)**:  
+    Criação ou seleção de características relevantes para melhorar o desempenho do modelo. Isso pode incluir transformar variáveis categóricas em numéricas, combinar variáveis existentes ou criar novas variáveis a partir dos dados disponíveis.
+    """)
+    if st.button('Voltar para a página principal'):
+        st.session_state.page = 'main'
--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 import streamlit as st
 
 from SubPaginas.Metricas import main as Metricas
+from SubPaginas.PreProcessamento import main as PreProcess
 
 
 def main_page():
     st.title("Maykoll Rocha - Tópicos em Aprendizagem de Máquina")
     st.markdown("""
-            ### Introdução 
-            
-            A seguir irei apressentar conteudo trazio para mim em tópicos em aprendizagem de máquina uma matéria muito relevante para minha futura 
-            vontade de vocação que será focada em análise de Dados.  
-            Será sudivido em aparte as apresentações com a Side Bar ao lado. Nele terá os projetos que fiz e os códigos que implementei.
+    ### Introdução  
+    Neste projeto, apresentarei conteúdos relacionados à disciplina de Aprendizado de Máquina, uma área essencial para a minha futura carreira, que será focada em Análise de Dados. O conteúdo será organizado de forma estruturada, com uma barra lateral para facilitar a navegação, onde você encontrará os projetos que desenvolvi e os códigos que implementei.  
+    Quando falamos de **Machine Learning**, é importante seguir etapas fundamentais. A primeira é a definição do problema e a segunda é a coleta de dados. Embora este projeto não trate especificamente dessas etapas iniciais, o foco aqui será em métricas, explicando a importância de utilizá-las e mostrando os códigos que criei para aplicá-las. No entanto, vale ressaltar que as duas primeiras fases são cruciais para a aplicação real de **Machine Learning**.  
             """)
+    st.markdown("""
+            ### . Pré-processamento dos Dados
+            Uma etapa crucial após obter os dados é processá-los corretamente. Muitas vezes, utilizam-se mecanismos de ETL (Extração, Transformação e Carregamento) para preparar os dados de forma adequada para o negócio ou aplicação desejada. No entanto, durante esse processo, erros inesperados podem ocorrer, uma vez que o ETL não foca necessariamente na limpeza dos dados. É comum que, após essa etapa, ainda haja duplicatas, valores inconsistentes ou variáveis que precisam ser ajustadas, como a necessidade de normalização e padronização.  
+            Por isso, o pré-processamento torna-se essencial para garantir que os dados estejam limpos e prontos para o uso em modelos de Machine Learning ou outras análises. Na próxima seção, abordaremos essas etapas detalhadamente.  
+            """)
+    
 
+    if st.button('Pré Processamento'):
+        st.session_state.page = 'preProcessamento'
     
     if st.button('Metricas'):
         st.session_state.page = 'Metricas'
+        
         
     
 # Defina o estado da página se não estiver definido
@@ -25,12 +33,9 @@ if 'page' not in st.session_state:
 
 
 # Controle de navegação
-if st.session_state.page == 'main':
-    main_page()
-elif st.session_state.page == 'Metricas':
-    Metricas()
-else:
-    main_page()
-    st.write("""
-             Ainda está em desenvolvimento
-             """)
+match st.session_state.page:
+    case 'main': main_page()
+    case 'Metricas': Metricas()
+    case "preProcessamento":PreProcess()
+    case _:
+        main_page()
