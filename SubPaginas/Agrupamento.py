31fb7a83676eeaf3902b37ff51a1d3e2f604d8a5
Maykoll Rocha <maykoll1412@gmail.com>
2024-09-15 13:23:53 -0400

    Iniciando

--- SubPaginas/Agrupamento.py
+++ SubPaginas/Agrupamento.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
 import streamlit as st
 
 from SubPaginas.Global import *
@@ -51,7 +54,7 @@ onde:
 
 O cálculo de $( P(n, k) )$ pode ser computacionalmente intensivo para grandes valores de $( n )$ e $( k )$, devido à necessidade de calcular potências e coeficientes binomiais. A complexidade computacional depende de como esses cálculos são realizados, geralmente envolvendo operações exponenciais e fatoriais. No entanto, para valores pequenos de $( n )$ e $( k )$, a fórmula é prática e fornece o número exato de particionamentos distintos possíveis.
                 """)
-    st.code(r"""
+    st.code(r'''
 # Função para calcular o fatorial de um número
 fat = lambda n: 1 if n == 0 else n * fat(n-1)
 
@@ -59,7 +62,7 @@ fat = lambda n: 1 if n == 0 else n * fat(n-1)
 arj = lambda n, k: fat(n) / (fat(k) * fat(n - k))
 
 def possibilidades(n, k):
-    \"""
+    """
     Calcula o número de possibilidades de k objetos em n posições com repetição,
     usando o princípio de inclusão-exclusão.
 
@@ -69,8 +72,48 @@ def possibilidades(n, k):
 
     Returns:
     - float: Número de possibilidades.
-    \"""
+    """
     return (1 / fat(k)) * sum([((-1) ** (k - j)) * arj(k, j) * (j ** n) for j in range(1, k + 1)])
-            """,language="python",line_numbers=True,wrap_lines=True)
+            ''',language="python",line_numbers=True,wrap_lines=True)
+    
+    
+    st.markdown('''
+            ## Objetivos e Características do Agrupamento de Dados
+
+            O agrupamento de dados é uma técnica essencial em análise de dados que visa identificar e descrever grupos naturais dentro de um conjunto de dados não rotulado. Aqui estão alguns dos principais objetivos e características dessa técnica:
+
+            1. **Trabalho com Dados Não Rotulados**: No agrupamento, os dados não possuem rótulos predefinidos que indiquem a que grupo ou classe pertencem. O objetivo é descobrir essas classes ou grupos com base nas características dos dados.
+
+            2. **Identificação e Descrição de Grupos**: Algoritmos de agrupamento são utilizados para identificar grupos de objetos semelhantes e descrever as características desses grupos. Cada objeto é representado como um ponto em um espaço vetorial, onde a dimensão do espaço é determinada pelo número de atributos do objeto.
+
+            3. **Uso de Protótipos**: Alguns métodos de agrupamento utilizam protótipos, que são vetores representativos para cada grupo. Esses protótipos ajudam a reduzir o tamanho da base de dados e o custo de processamento, além de facilitar a análise das características dos grupos. Protótipos geralmente são posicionados no centro dos grupos ou em regiões de alta densidade, maximizando a representatividade dos objetos agrupados.
+
+            4. **Redução e Representatividade**: O uso de protótipos permite condensar grandes quantidades de dados em um número menor de representações, mantendo a integridade das informações. Isso ajuda a simplificar a análise e interpretar os grupos encontrados de forma mais eficiente.
+
+            5. **Descoberta de Grupos Naturais**: O objetivo final do agrupamento é identificar grupos naturais dentro dos dados. Esses grupos representam padrões ou estruturas subjacentes que podem fornecer insights valiosos sobre a base de dados.
+
+            Em resumo, o agrupamento busca explorar dados não rotulados para identificar e entender padrões ocultos, utilizando métodos que podem incluir a criação de protótipos para facilitar a análise e interpretação dos resultados.
+                
+            ## Grupos Naturais
+
+            Grupos naturais são definidos por dois critérios principais, conforme descrito por Carmichael (1968):
+
+            1. **Regiões Contínuas e Densas**: Existem regiões contínuas no espaço de dados que são relativamente densamente povoadas por objetos. Essas regiões formam agrupamentos de objetos que são mais numerosos e próximos uns dos outros do que com objetos fora da região.
+
+            2. **Áreas Relativamente Vazias**: As regiões densas estão rodeadas por áreas do espaço que são relativamente vazias. Isso significa que há uma clara separação entre os grupos, com poucas ou nenhuma interseção significativa entre as regiões densas e as regiões ao redor.
+
+            Esses grupos naturais representam padrões subjacentes nos dados e são úteis para a análise e interpretação dos dados, ajudando a identificar estruturas e relações significativas.    
+                
+                ''')
+    # Baixa os dados dos arquivos
+    data_response = requests.get('https://raw.githubusercontent.com/MaykollRocha/Data_Sets/main/data.txt')
+    rotulo_response = requests.get('https://raw.githubusercontent.com/MaykollRocha/Data_Sets/main/rotulos.txt')
+
+    # Carrega os dados no NumPy
+    data = np.loadtxt(data_response.text.splitlines())
+    rotulo = np.loadtxt(rotulo_response.text.splitlines())
+    plt.scatter(data[:,0], data[:,1], c=rotulo)
+    plt.show()
+    
     if st.button('Voltar para a página principal'):
         st.session_state.page = 'main'
