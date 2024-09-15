c5c3fc870e133ce863d3bf8e62e6d6b8600fd136
Maykoll Rocha <maykoll1412@gmail.com>
2024-09-15 11:26:51 -0400

    Iniciando

--- SubPaginas/Global.py
+++ SubPaginas/Global.py
@@ -7,15 +7,15 @@ A normalização min-max transforma os dados para que todos os valores estejam e
 
 A fórmula para a normalização min-max é:
 
-$
+$$
 x_{\text{norm}} = \frac{x - \text{min}(X)}{\text{max}(X) - \text{min}(X)}
-$
+$$
 
 Onde:
-- \( x_{\text{norm}} \) é o valor normalizado,
-- \( x \) é o valor original,
-- \(\text{min}(X)\) é o valor mínimo do conjunto de dados \(X\),
-- \(\text{max}(X)\) é o valor máximo do conjunto de dados \(X\).
+- $\( x_{\text{norm}} \)$ é o valor normalizado,
+- $\( x \)$ é o valor original,
+- $\(\text{min}(X)\)$ é o valor mínimo do conjunto de dados $\(X\)$,
+- $\(\text{max}(X)\)$ é o valor máximo do conjunto de dados $\(X\)$.
 
 ### Interpretação
 
@@ -81,11 +81,11 @@ f(X) = \frac{X - \text{min}_X}{\text{max}_X - \text{min}_X} \times (\text{novo\_
 $$
 
 Onde:
-- \( X \) é o valor original,
-- \(\text{min}_X\) é o valor mínimo do conjunto de dados original,
-- \(\text{max}_X\) é o valor máximo do conjunto de dados original,
-- \(\text{novo\_min}_X\) é o novo valor mínimo desejado para o intervalo,
-- \(\text{novo\_max}_X\) é o novo valor máximo desejado para o intervalo.
+- $\( X \)$ é o valor original,
+- $\(\text{min}_X\)$ é o valor mínimo do conjunto de dados original,
+- $\(\text{max}_X\)$ é o valor máximo do conjunto de dados original,
+- $\(\text{novo\_min}_X\)$ é o novo valor mínimo desejado para o intervalo,
+- $\(\text{novo\_max}_X\)$ é o novo valor máximo desejado para o intervalo.
 
 ### Interpretação
 
@@ -107,14 +107,14 @@ Essa técnica escala os dados dividindo cada valor pelo valor absoluto máximo d
 
 A fórmula para a normalização pelo valor máximo é:
 
-\[
+$$
 x' = \frac{x}{|x_{\text{max}}|}
-\]
+$$
 
 Onde:
-- \( x' \) é o valor normalizado,
-- \( x \) é o valor original,
-- \( |x_{\text{max}}| \) é o valor absoluto do maior valor absoluto no conjunto de dados.
+- $\( x' \)$ é o valor normalizado,
+- $\( x \)$ é o valor original,
+- $\( |x_{\text{max}}| \)$ é o valor absoluto do maior valor absoluto no conjunto de dados.
 
 ### Interpretação
 
